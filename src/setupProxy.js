201561907eb96ec2d3d51c020bdbef07dc8ab139
pazbear <mkcho1997@daum.net>
2020-10-08 17:42:04 +0900

    t4

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
-        createProxyMiddleware({
+        proxy('/api',{
             target: 'https://appdearborn.herokuapp.com',
+            changeOrigin: true,
         })
     );
 };
